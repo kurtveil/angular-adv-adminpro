92d69244879612a9bf866fd01ba746ae7a7bd717
Ani <nutrisite1@gmail.com>
2022-09-06 16:58:27 +0200

    cambio de color navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -14,16 +15,20 @@ import { HeaderComponent } from './header/header.component';
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
   ],
   exports: [
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
+    FooterComponent
+
   ]
 })
-export class SharedModule { }
+export class SharedModule { 
+}
